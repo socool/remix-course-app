9795ba139747ce80b5723541a12257ed2904be3c
So Cool <127.0.0.1.pm@gmail.com>
2024-01-23 11:32:13 +0700

    add styles to auth and expenses

--- app/routes/auth.jsx
+++ app/routes/auth.jsx
@@ -1,3 +1,8 @@
+import authStyles from "~/styles/auth.css";
 export default function AuthPage() {
   return <h1>Auth Page</h1>;
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: authStyles }];
 }
