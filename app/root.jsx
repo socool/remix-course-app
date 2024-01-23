42e423f5a4307a63a0441795d8dc9a8af20b6424
So Cool <127.0.0.1.pm@gmail.com>
2024-01-23 11:35:15 +0700

    add font

--- app/root.jsx
+++ app/root.jsx
@@ -19,8 +19,13 @@ export default function App() {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
         <Meta />
         <Links />
       </head>
