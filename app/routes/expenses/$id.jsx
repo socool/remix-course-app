863f00d648758b14125eebde51c9b8f2f4e23bf8
So Cool <127.0.0.1.pm@gmail.com>
2024-01-23 14:45:41 +0700

    add onClose on Modal

--- app/routes/expenses/$id.jsx
+++ app/routes/expenses/$id.jsx
@@ -1,10 +1,15 @@
 // /expenses/<some-id> => /expenses/expense-1, /expenses/e-1
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
+import { useNavigate } from "@remix-run/react";
 
 export default function UpdateExpensesPage() {
+  const navigate = useNavigate();
+  function closeHandler() {
+    navigate("..");
+  }
   return (
-    <Modal>
+    <Modal onClose={closeHandler}>
       <ExpenseForm />
     </Modal>
   );
--- app/routes/expenses/add.jsx
+++ app/routes/expenses/add.jsx
@@ -1,12 +1,18 @@
 // /expense/add
+import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
 
 export default function AddExpensesPage() {
+  const navigate = useNavigate();
+  useNavigate();
+  function closeHandler() {
+    // navigate programmatically
+    navigate("..");
+  }
   return (
-    <Modal>
-      {" "}
-      <ExpenseForm />{" "}
+    <Modal onClose={closeHandler}>
+      <ExpenseForm />
     </Modal>
   );
 }
